cf507faa27012de4f41a5c963061ac431c2d6085
benjamineruvieru <benjamineruvieru@gmail.com>
2023-04-10 12:58:26 +0100

    delete

--- cli/newconsole.py
+++ cli/newconsole.py
@@ -31,93 +31,94 @@ def main(stdscr):
     input_str = ''
     globalstate.message_win = message_win
     globalstate.input_win = input_win
-    # try:
-    while globalstate.RUNNING:
-        ch = input_win.getch()
-        log(f'c {ch} chr{chr(ch)}', message_win)
-        if ch == curses.KEY_EXIT or ch == 3:  # CTRL+C
-            del message_win
-            del input_win
-            curses.endwin()
-            break
-        elif ch == curses.KEY_ENTER or ch == 10:  # Enter key
-            input_win.clear()
-            input_win.addstr("> ")
-            process_commands(input_str,
-                             message_win, input_win)
-            input_win.clear()
-            input_win.addstr("> ")
-            input_win.addstr(f'{globalstate.PLACEHOLDER}...', GREY)
-            input_win.move(input_win.getyx()[0], 2)
-            input_win.refresh()
-            input_str = ''
-        elif ch == -1:  # Terminal resized
-            # height, width = stdscr.getmaxyx()
-            # message_win.resize(height - 2, width)
-            # input_win.mvwin(height - 1, 1)
-            # input_win.resize(1, width - 2)
-            pass
-        # check if printable ASCII character
-        elif ch and ch < 256 and chr(ch).isprintable():
-            x = input_win.getyx()[1]
-            input_list = list(input_str)
-            input_list.insert(x-2, chr(ch))
-            input_str = ''.join(input_list)
-            input_win.clear()
-            if len(input_str) < width-3:
-                input_win.addstr(f'> {input_str}')
-                input_win.move(input_win.getyx()[0], x+1)
+    try:
+        while globalstate.RUNNING:
+            ch = input_win.getch()
+            # log(f'c {ch} chr{chr(ch)}', message_win)
+            if ch == curses.KEY_EXIT or ch == 3:  # CTRL+C
+                del message_win
+                del input_win
+                curses.endwin()
+                break
+            elif ch == curses.KEY_ENTER or ch == 10:  # Enter key
+                input_win.clear()
+                input_win.addstr("> ")
+                process_commands(input_str,
+                                 message_win, input_win)
+                input_win.clear()
+                input_win.addstr("> ")
+                input_win.addstr(f'{globalstate.PLACEHOLDER}...', GREY)
+                input_win.move(input_win.getyx()[0], 2)
                 input_win.refresh()
+                input_str = ''
+            elif ch == -1:  # Terminal resized
+                height, width = stdscr.getmaxyx()
+                message_win.mvwin(0, 0)
+                message_win.resize(height - 2, width)
+                input_win.mvwin(height - 1, 0)
+                input_win.resize(1, width)
 
-            else:
-                input_win.addstr(f'> {input_str[-1*(width-3):]}')
-                input_win.refresh()
-
-        elif ch == curses.KEY_DC or ch == 127 or ch == curses.KEY_BACKSPACE:  # delete key
-            x = input_win.getyx()[1]
-            input_list = list(input_str)
-            if len(input_list) == 0:
-                pass
-            elif x-3 < 0:
-                pass
-            elif x-3 > len(input_list)-1:
-                pass
-            else:
-                del input_list[x-3]
+            # check if printable ASCII character
+            elif ch and ch < 256 and chr(ch).isprintable():
+                x = input_win.getyx()[1]
+                input_list = list(input_str)
+                input_list.insert(x-2, chr(ch))
                 input_str = ''.join(input_list)
                 input_win.clear()
-                if len(input_list) == 0:
-                    input_win.addstr("> ")
-                    input_win.addstr(f'{globalstate.PLACEHOLDER}...', GREY)
-                    input_win.move(input_win.getyx()[0], 2)
-                    input_win.refresh()
-                elif len(input_str) >= width-3:
-                    input_win.addstr(f'> {input_str[-1*(width-3):]}')
+                if len(input_str) < width-3:
+                    input_win.addstr(f'> {input_str}')
+                    input_win.move(input_win.getyx()[0], x+1)
                     input_win.refresh()
 
                 else:
-                    input_win.addstr(f'> {input_str}')
-                    input_win.move(input_win.getyx()[0], x-1)
+                    input_win.addstr(f'> {input_str[-1*(width-3):]}')
                     input_win.refresh()
-        elif ch == curses.KEY_LEFT:
-            if input_win.getyx()[1] > 2:
-                input_win.move(input_win.getyx()[
-                    0], input_win.getyx()[1] - 1)
 
-        elif ch == curses.KEY_RIGHT:
-            log('y', message_win)
-            if input_win.getyx()[1] < input_win.getmaxyx()[1] - 1:
-                input_win.move(input_win.getyx()[
-                    0], input_win.getyx()[1] + 1)
-        elif ch == curses.KEY_UP:
-            message_win.scroll(1)
-            message_win.refresh()
-        elif ch == curses.KEY_DOWN:
-            message_win.scroll(-1)
-            message_win.refresh()
+            elif ch == curses.KEY_DC or ch == 127 or ch == 8 or ch == curses.KEY_BACKSPACE:  # delete key
+                x = input_win.getyx()[1]
+                input_list = list(input_str)
+                if len(input_list) == 0:
+                    pass
+                elif x-3 < 0:
+                    pass
+                elif x-3 > len(input_list)-1:
+                    pass
+                else:
+                    del input_list[x-3]
+                    input_str = ''.join(input_list)
+                    input_win.clear()
+                    if len(input_list) == 0:
+                        input_win.addstr("> ")
+                        input_win.addstr(f'{globalstate.PLACEHOLDER}...', GREY)
+                        input_win.move(input_win.getyx()[0], 2)
+                        input_win.refresh()
+                    elif len(input_str) >= width-3:
+                        input_win.addstr(f'> {input_str[-1*(width-3):]}')
+                        input_win.refresh()
 
-    # except:
-    #     pass
+                    else:
+                        input_win.addstr(f'> {input_str}')
+                        input_win.move(input_win.getyx()[0], x-1)
+                        input_win.refresh()
+            elif ch == curses.KEY_LEFT:
+                if input_win.getyx()[1] > 2:
+                    input_win.move(input_win.getyx()[
+                        0], input_win.getyx()[1] - 1)
+
+            elif ch == curses.KEY_RIGHT:
+                log('y', message_win)
+                if input_win.getyx()[1] < input_win.getmaxyx()[1] - 1:
+                    input_win.move(input_win.getyx()[
+                        0], input_win.getyx()[1] + 1)
+            elif ch == curses.KEY_UP:
+                message_win.scroll(1)
+                message_win.refresh()
+            elif ch == curses.KEY_DOWN:
+                message_win.scroll(-1)
+                message_win.refresh()
+
+    except:
+        pass
     curses.endwin()
 
 
